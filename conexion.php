6af44bd3285cf266228d81acb438055539653765
bombasin111 <bombasin111@gmail.com>
2025-06-25 10:34:18 -0400

    Dockerfi

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ $user = getenv('DB_USER');
 $password = getenv('DB_PASS');
 
 try {
-    $conexion = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $password);
+    $conexion = new PDO("pgsql:host=$host;port=6543;dbname=$dbname", $user, $password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
